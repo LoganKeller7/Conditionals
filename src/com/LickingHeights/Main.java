b0741a55a298445ef4ef1c9911a0040d28904e79
Logan Keller <logan.keller.2023@student.lhschools.org>
2020-03-12 12:15:46 -0400

    Conditional implemented

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,22 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int numberA = 10;
+
+	if(numberA != 10){
+    System.out.println("the number is not equal to 10");
+    }
+
+    else if(numberA == 10){
+	    System.out.println("the number is equal to 10");
+    }
+
+    else{
+	    System.out.println("The number is smaller than 10");
+    }
+
+    }
+}
